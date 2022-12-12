2c884dc9e19cdcffe8527107dc034e1f7e5cdcb2
García González <mgarciago@minsait.com>
2022-12-12 15:03:04 +0100

    Environment fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
     production: true,
+    baseUrl: "https://www.oysho.com/itxrest/2/catalog/store/64009600/60361120",
+    imageBaseUrl: "https://static.oysho.net/6/photos2",
   };
