7314c4ac3f52c820202298d7addef911d3bd5e17
García González <mgarciago@minsait.com>
2023-01-16 12:48:31 +0100

    First commit

--- src/app/shared/pipes/filter-product.pipe.spec.ts
+++ src/app/shared/pipes/filter-product.pipe.spec.ts
@@ -1,39 +1,16 @@
 import { SpectatorPipe, createPipeFactory } from '@ngneat/spectator';
 import { Products } from 'src/app/core/services/products/models/products.interface';
 import { FilterProductPipe } from './filter-product.pipe';
+import { CategoriesComponent } from '../../pages/categories/categories.component';
 
 describe('FilterProductPipe', () => {
-  let spectator: SpectatorPipe<FilterProductPipe>;
+  let spectator: SpectatorPipe<FilterProductPipe, CategoriesComponent>;
   const createPipe = createPipeFactory(FilterProductPipe);
 
-  it('create an instance', () => {
+  it('should exist', () => {
+    spectator = createPipe();
     expect(spectator.element).toBeTruthy();
-  });
-
-  it('should transform value of filter to lowerCase and filter the array', () => {
-    const list = [
-      {
-        name: 'Bra',
-      },
-      {
-        name: 'T-Shirt',
-      },
-      {
-        name: 'Skirt',
-      },
-      {
-        name: 'Dress',
-      },
-      {
-        name: 'Pants',
-      }
-    ];
-
-    let filter: string = 'Bra ';
-    let filteredProducts: Products[] = [];
-
-    spectator = createPipe(`{{list | filterProduct: filter}}`)
-    expect(filter).toEqual('bra');
-    expect(filteredProducts).toContain('bra')
   })
+
+
 });
